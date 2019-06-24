b9fd766cde226fd7fb6d805e715cc28083aab451
libang <bang@163.com>
2019-06-24 16:31:17 +0800

    update 02

---
+++ src/main/java/com/atguigu/MyWork.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author Bang
+ * @create 2019-06-24 16:30
+ */
+public class MyWork {
+    public static void main(String[] args) {
+        System.out.println(" my work !!! ");
+    }
+}
