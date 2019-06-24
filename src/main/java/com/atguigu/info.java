289126f949308e9dd7667f1e01d68886c8326cee
libang <Bang@163.com>
2019-06-24 16:15:44 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/info.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author Bang
+ * @create 2019-06-24 16:14
+ */
+public class info {
+    public static void main(String[] args) {
+        System.out.println("name : zhangsan , age : 21");
+    }
+}
